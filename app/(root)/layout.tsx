22169c9ac1ed5bef33ccb22e4063b488b9a15e70
tanvi <101176974+shindodkar@users.noreply.github.com>
2024-12-11 14:34:09 +0530

    meeting

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import StreamVideoProvider from "@/Providers/StreamClientProvider";
+import React, { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+
+const Meeting = ({ params }: { params: { id: string } }) => {
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const { user, isLoaded } = useUser();
+  return (
+    <main className="h-screen w-full">
+      <StreamCall>
+        <StreamTheme>
+          {isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default Meeting;
